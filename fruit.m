21c71f2866addadf651655c73f8ba8a62a1a1883
Tyler31484 <tylerty.green@slu.edu>
2024-11-11 10:58:07 -0600

    Update fruit.m

--- fruit.m
+++ fruit.m
@@ -4,4 +4,19 @@ function [] = fruit( N )
 % “banana” if the value is divisible by 5
 % The number itself if neither is true
 % “applebanana” if both are true
-
+%%
+function [] = fruit(N)
+    for i = 1:N
+        if mod(i, 2) == 0 && mod(i, 5) == 0
+            disp('applebanana');
+        elseif mod(i, 2) == 0
+            disp('apple');
+        elseif mod(i, 5) == 0
+            disp('banana');
+        else
+            disp(i);
+        end
+    end
+end
+&&
+input : fruit(n)
